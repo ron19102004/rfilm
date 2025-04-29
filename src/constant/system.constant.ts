a1e68e4f7e31ac894b7c300d27db81deb4af770a
Your Name <you@example.com>
2025-04-29 15:47:00 +0700

    update

--- src/constant/system.constant.ts
+++ src/constant/system.constant.ts
@@ -1,2 +1,2 @@
 export const ENDPOINT_WEB ="https://rfilm.netlify.app"
-export const URL_DOWNLOAD_APP_ANDROID = "https://drive.google.com/uc?export=download&id=1Y-SD-irTjFFfNotP_g3N__6xhxrSQGET";
+export const URL_DOWNLOAD_APP_ANDROID = "https://drive.google.com/uc?export=download&id=1ivZESh0gRoa9aGWkJlrURfMOsx7mRX6L";
